48982aaa1e8090722c534802bb76b4a58f1cb51b
PaulGRanadosRodriguez <paul.granados.rodriguez@est.una.ac.cr>
2026-02-04 01:06:28 -0600

    dashboard funcional con lista de prueba, arreglado el error de posición en cambio de variable

--- src/main/java/com/sistema/ordenes/DashboardController.java
+++ src/main/java/com/sistema/ordenes/DashboardController.java
@@ -1,5 +1,39 @@
 package com.sistema.ordenes;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.security.PrivateKey;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class DashboardController {
     
+    @FXML private TableView <Product> tableProducts;
+    @FXML private TableColumn <Product, Integer> colId;
+    @FXML private TableColumn <Product,String> colName;
+    @FXML private TableColumn <Product,Integer> colStock;
+    @FXML private TableColumn <Product, Double> colPrice;
+
+    @FXML
+    public void initialize(){
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colStock.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        colPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        
+        //Lista de prueba
+        ObservableList<Product>data = FXCollections.observableArrayList(
+            new Product(1,"Laptop", 10, 1500.0),
+            new Product(2, "Mouse", 50, 25.0),
+            new Product(3,"Teclado", 30 , 45.0)
+        );
+
+        tableProducts.setItems(data);
+
+    }
+
 }
--- src/main/java/com/sistema/ordenes/Product.java
+++ src/main/java/com/sistema/ordenes/Product.java
@@ -1,30 +1,33 @@
 package com.sistema.ordenes;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 public class Product {
-    private String id;
-    private String name;
-    private double price;
-    private int stock;
+    private final SimpleIntegerProperty id;
+    private final SimpleStringProperty name;
+    private final SimpleDoubleProperty price;
+    private final SimpleIntegerProperty stock;
 
 
-    public Product(String id, String name, double price, int stock){
-        this.id=id;
-        this.name=name;
-        this.price=price;
-        this.stock=stock;
+    public Product(int id, String name, int stock,double price ){
+        this.id= new SimpleIntegerProperty(id);
+        this.name= new SimpleStringProperty(name);
+        this.stock= new SimpleIntegerProperty(stock);
+        this.price= new SimpleDoubleProperty(price);
 
     }
 
-    public String getId(){
-        return id;
+    public int getId(){
+        return id.get();
     }
     public String getName(){
-        return name;
+        return name.get();
     }
     public double getPrice(){
-        return price;
+        return price.get();
     }
     public int getStock(){
-        return stock;
+        return stock.get();
     }
 
     @Override
